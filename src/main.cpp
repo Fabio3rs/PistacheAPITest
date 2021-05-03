fb5ac73b2424cb37c02b860efd4957e8007037f9
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2021-05-02 21:31:31 -0300

    Port on env

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,19 @@ int main(int argc, char* argv[], char* envp[])
     auto& conf = CConfig::config();
     conf.load_from_envp(envp);
 
-    conf.at("teste", "adsw");
+    int config_port = 9000;
+
+    {
+        std::string PORT = conf.at("PORT", "9000");
+        try
+        {
+            config_port = std::stoi(PORT, nullptr, 0);
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
 
     // API Controllers setup
     std::unique_ptr<CWelcomeController> control(std::make_unique<CWelcomeController>());
@@ -24,7 +36,7 @@ int main(int argc, char* argv[], char* envp[])
     endp.register_controller(std::move(control));
     endp.register_controller(std::move(crudtest));
 
-    Pistache::Port port(9080);
+    Pistache::Port port(config_port);
     Pistache::Address addr(Pistache::Ipv4::any(), port);
     endp.init(addr);
     endp.start();
